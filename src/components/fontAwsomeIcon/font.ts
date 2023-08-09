159cf29e2559c9fac1871a0de433be527dd1bc50
Nguyễn Hải Dân <Nguyenhaidan13112000@gmail.com>
2023-08-09 10:53:53 +0700

    font-icon-config

--- src/components/fontAwsomeIcon/font.ts
+++
@@ -1,4 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faEnvelope, faEyeSlash,faEye } from "@fortawesome/free-solid-svg-icons";
-
-library.add(faEnvelope, faEyeSlash,faEye)
